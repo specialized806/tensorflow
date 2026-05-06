f72c688c8bdd65c4f7a45e20a75b2596ece7fc2e
Eugene Zhulenev <ezhulenev@openxla.org>
2026-05-05 22:16:41 -0700

    PR #41561: [tsl:concurrency] Add automatic Future::Map flattening
    Imported from GitHub PR https://github.com/openxla/xla/pull/41561
    
    Add support for automatic mapping of Future<Future<T>> to Future<T>
    Copybara import of the project:
    
    --
    45a944f714bb602a4e5d36d2b883872f2da261aa by Eugene Zhulenev <ezhulenev@openxla.org>:
    
    [tsl:concurrency] Add automatic Future::Map flattening
    
    Merging this change closes #41561
    
    PiperOrigin-RevId: 911105995

--- tensorflow/core/tfrt/ifrt/ifrt_serving_executable.cc
+++ tensorflow/core/tfrt/ifrt/ifrt_serving_executable.cc
@@ -1221,22 +1221,20 @@ IfrtServingExecutable::ExecuteAsync(
     return final_future;
   }
 
-  return status_future
-      .Map([final_future = std::move(final_future),
-            exec_info = std::move(exec_info),
-            thread_pool = &thread_pool_]() mutable
-               -> tsl::Future<std::vector<tensorflow::Tensor>> {
-        return final_future.Map(
-            [exec_info = std::move(exec_info),
-             thread_pool](std::vector<tensorflow::Tensor> outputs) mutable {
-              // Offload cleanup to thread pool to avoid blocking execution
-              // thread.
-              thread_pool->Schedule(
-                  [exec_info = std::move(exec_info)]() mutable {});
-              return outputs;
-            });
-      })
-      .Flatten();
+  return status_future.Map([final_future = std::move(final_future),
+                            exec_info = std::move(exec_info),
+                            thread_pool = &thread_pool_]() mutable
+                               -> tsl::Future<std::vector<tensorflow::Tensor>> {
+    return final_future.Map(
+        [exec_info = std::move(exec_info),
+         thread_pool](std::vector<tensorflow::Tensor> outputs) mutable {
+          // Offload cleanup to thread pool to avoid blocking execution
+          // thread.
+          thread_pool->Schedule(
+              [exec_info = std::move(exec_info)]() mutable {});
+          return outputs;
+        });
+  });
 }
 
 absl::Status IfrtServingExecutable::AsyncLoadIfrtArray(
